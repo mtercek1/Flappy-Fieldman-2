694ee9566699048a8a4b3c15a72355b72a0b2259
Charlie Tercek <Smith@smiths-mbp-3.internal.hws.org>
2015-03-24 09:00:59 -0700

    Initial Commit

---
+++ Flappy Fieldman/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Flappy Fieldman
+//
+//  Created by Charlie Tercek on 3/24/15.
+//  Copyright (c) 2015 GirlCode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Flappy Fieldman/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Flappy Fieldman
+//
+//  Created by Charlie Tercek on 3/24/15.
+//  Copyright (c) 2015 GirlCode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
